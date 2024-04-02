ae2bcfce88599df75c558441c79846ac35f5d44d
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-04-02 16:13:19 +0530

    Update main.go

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
     "github.com/gin-gonic/gin"
 )
 
-
+type Page struct {
+        Images []string
+}
 
 func main() {
     // Initialize Gin router
@@ -14,12 +16,26 @@ func main() {
     // Serve static files (HTML, CSS, JS)
     router.Static("/static", "./static")
 
-    //images := []string{"image1.jpg", "image2.jpg", "image3.jpg"}
+            matches, err := filepath.Glob(filepath.Join("static", "images", "baked_goods_*.jpeg"))
+        if err != nil {
+                log.Printf("Error loading baked goods images: %v", err)
+        }
+        var page = &Page{Images: make([]string, len(matches))}
+        for i, match := range matches {
+                page.Images[i] = filepath.Base(match)
+        }
+        switch r.URL.Path {
+        case "/":
+                err = tmpl.Execute(w, page)
+                if err != nil {
+                        log.Printf("Template execution error: %v", err)
+                }
+        }
 
     // Define routes
     router.GET("/", func(c *gin.Context) {
         c.HTML(http.StatusOK, "index.html", gin.H{
-            //"Images": "image1.jpg",
+            "Images": "image1.jpg",
         })
     })
 
