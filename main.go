05380f1573b8b182c40436cacdf2b908d8058245
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-04-02 15:51:17 +0530

    Update main.go

--- main.go
+++ main.go
@@ -5,16 +5,24 @@ import (
     "github.com/gin-gonic/gin"
 )
 
+type PageData struct {
+    Images []string
+}
+
 func main() {
     // Initialize Gin router
     router := gin.Default()
 
     // Serve static files (HTML, CSS, JS)
-    router.Static("/static", "./static")
+    router.Static("/", "./static")
+
+    images := []string{"image1.jpg", "image2.jpg", "image3.jpg"}
 
     // Define routes
     router.GET("/", func(c *gin.Context) {
-        c.HTML(http.StatusOK, "index.html", gin.H{})
+        c.HTML(http.StatusOK, "index.html", gin.H{
+            "Images": images,
+        })
     })
 
     // Start server
