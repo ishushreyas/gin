993ade335b521a249787945c1b1b2049bb4196ad
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-04-02 16:02:29 +0530

    Update main.go

--- main.go
+++ main.go
@@ -16,12 +16,12 @@ func main() {
     // Serve static files (HTML, CSS, JS)
     router.Static("/", "./static")
 
-    images := []string{"image1.jpg", "image2.jpg", "image3.jpg"}
+    //images := []string{"image1.jpg", "image2.jpg", "image3.jpg"}
 
     // Define routes
     router.GET("/", func(c *gin.Context) {
         c.HTML(http.StatusOK, "index.html", gin.H{
-            "Images": images,
+            "Images": "image1.jpg",
         })
     })
 
