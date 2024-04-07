b85ab639f3d0c524651d5f26b91c997484d79f24
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-04-07 17:02:21 +0530

    Update main.go

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
     router := gin.Default()
 
     // Serve static files (HTML, CSS, JS)
-    router.LoadHTMLGlob("static/*")
+    router.LoadHTMLGlob("static/*.html")
     //router.Static("/static", "./static")
 
     images := []string{"image1.jpg", "image2.jpg", "image3.jpg"}
