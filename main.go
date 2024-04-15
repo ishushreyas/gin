1fe9f1e518d68c217b44c6868aaa5eb6097068f6
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-04-15 12:49:13 +0530

    Update main.go

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
-	r.LoadHTMLFiles("/templates/index.html")
+	r.LoadHTMLFiles("/templates/index.tmpl.html")
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+		c.JSON(200, "index.tmpl.html", gin.H{
 			"message": "pong",
 		})
 	})
